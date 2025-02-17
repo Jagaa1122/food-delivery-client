5b9e9552f5ea8927d515c138fd5ec77362264d01
Jagaa1122 <chjagaa@icloud.com>
2025-02-17 12:56:16 +0800

    first commit

---
+++ src/util/types.ts
@@ -0,0 +1,57 @@
+type Food = {
+  _id: string;
+  foodName: string;
+  price: number;
+  image: string;
+  ingredients: string;
+  category: string;
+  createdAt: Date;
+  updated: Date;
+};
+
+type FoodCategory = {
+  _id: string;
+  categoryName: string;
+  createdAt: Date;
+  updated: Date;
+};
+
+type FoodOrderItem = {
+  food: string;
+  quantity: number;
+};
+
+type FoodOrder = {
+  _id: string;
+  user: string;
+  totalPrice: number;
+  foodOrderItems: FoodOrderItem[];
+  status: FoodOrderStatusEnum;
+  createdAt: Date;
+  updated: Date;
+};
+
+type FoodOrderStatusEnum = {
+  PENDING: PENDING;
+  CANCELED: CANCELED;
+  DELIVERED: DELIVERED;
+};
+
+type User = {
+  _id: string;
+  email: string;
+  password: string;
+  phoneNumber: string;
+  address: string;
+  role: UserRoleEnum;
+  orderedFoods: string;
+  ttl: Date;
+  isVerified: Boolean;
+  createdAt: Date;
+  updated: Date;
+};
+
+type UserRoleEnum = {
+  USER: USER;
+  ADMIN: Admin;
+};
