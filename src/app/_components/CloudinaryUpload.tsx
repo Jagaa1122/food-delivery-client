edbad28186c9166cdaa684e63f3daf96cbac5b1b
Jagaa1122 <chjagaa@icloud.com>
2025-02-18 12:53:43 +0800

    Login dutuu

---
+++ src/app/_components/CloudinaryUpload.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const CloudinaryUpload = () => {
+  return (
+    <div>
+      <input type="file" />
+      <button></button>
+    </div>
+  );
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import LoginPage from "./_components/Login";
+
+export default function Home() {
+  return (
+    <div className="flex w-screen h-screen justify-center">
+      <LoginPage />
+    </div>
+  );
+}
