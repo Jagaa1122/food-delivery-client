84b0f08fa108fe1528c59456222f4c7bc2991b79
Jagaa1122 <chjagaa@icloud.com>
2025-02-23 23:52:21 +0800

    add

--- src/app/admin/asd/page.tsx
+++ src/app/admin/asd/page.tsx
@@ -1,15 +1,15 @@
 import { UploadCloudinary } from "@/app/_components/CloudinaryUpload";
 import { Button } from "@/components/ui/button";
-import React,{ ReactNode } from "react";
+import React, { ReactNode } from "react";
 
-const WrapperComponent = ({children}: {children: ReactNode}) => {
+const WrapperComponent = ({ children }: { children: ReactNode }) => {
     return (
-    <div>
-       <div className=" bg-white rounded-xl mb-[20px] p-[20px]">
-       <p className=" "> Dishes Category</p>
-        </div> 
-        {children}
-    </div>
+        <div>
+            <div className=" bg-white rounded-xl mb-[20px] p-[20px]">
+                <p className=" "> Dishes Category</p>
+            </div>
+            {children}
+        </div>
     );
 };
 
@@ -21,9 +21,20 @@ const Page = () => {
                     <h1>Appitizers</h1>
                     <div>
                         <div className=" flex justify-center items-center flex-col rounded-lg border-2 border-dashed border-[#EF4444] w-[25%] h-[241px]">
-                            <Button className="w-[40px] h-[40px] rounded-[20px] p-0 bg-[#EF4444]">+</Button>
+                            <label
+                                htmlFor="file-upload"
+                                className="w-[40px] h-[40px] flex justify-center items-center rounded-[20px] bg-[#EF4444] cursor-pointer text-white"
+                            >
+                                +
+                            </label>
+                            <input
+                                id="file-upload"
+                                type="file"
+                                className="hidden"
+                                accept="image/*"
+                            />
                             <p>Add a new dish to the Appitizers</p>
-                            
+
                         </div>
                     </div>
                 </div>
