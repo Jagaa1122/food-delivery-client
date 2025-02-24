407cea313df3de9b9b9673869d1237c3a3f91dfc
Jagaa1122 <chjagaa@icloud.com>
2025-02-24 13:10:16 +0800

    ddd

--- src/app/admin/_components/Logo.tsx
+++ src/app/admin/_components/Logo.tsx
@@ -1,20 +1,46 @@
+import { Toggle } from "@/components/ui/toggle";
 import Image from "next/image";
+import Link from "next/link";
+import { LayoutDashboard, Settings, Truck } from "lucide-react";
 
 export default function Logo() {
   return (
-    <div className="flex gap-3">
+    <div className="flex flex-col gap-3">
       <Image
-       className="w-[36px] h-[29px]"
+        className="w-[36px] h-[29px]"
         width={36}
         height={29}
         alt="logo"
         src="/menu.svg"
       />
       <div>
-        <h2 className="text-black">
+        <h2 className="text-black text-[18px] font-[600]">
           Nom<span className="text-black">Nom</span>
         </h2>
-        <p>Swift delivery</p>
+        <p className="text-[12px]">Swift delivery</p>
+      </div>
+      <div className="flex flex-col mt-10 gap-6">
+        <Link href="/foodmenu">
+          <div>
+            <Toggle className="h-10 w-[165px] rounded-full py-2 px-6 flex justify-start gap-[10px] ">
+              <LayoutDashboard />
+              Food menu
+            </Toggle>
+          </div>
+        </Link>
+        <Link href={"/orders"}>
+          <div>
+            <Toggle className="h-10 w-full flex py-2 px-6 justify-start gap-[10px] rounded-full">
+              <Truck />
+              Orders
+            </Toggle>
+          </div>
+        </Link>
+
+        <Toggle className="h-10 w-full py-2 px-6 flex justify-start gap-[10px] rounded-full">
+          <Settings />
+          Settings
+        </Toggle>
       </div>
     </div>
   );
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,22 +1,10 @@
-"use client"
+"use client";
 
 import { Sidebar } from "lucide-react";
 import Logo from "./_components/Logo";
 
-
-
-
 const Page = () => {
-  return (
-  <div className="flex">
-    
-    <p>Hello Test</p>
-    <Logo />
-    </div>
-  );
+  return <div className="flex"></div>;
 };
 
-
-
 export default Page;
-
